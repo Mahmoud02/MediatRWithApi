8fea64bcf8c6b444ef302a67d470425cf7f1871d
captano <mahmoudreda027@gmail.com>
2021-02-27 22:44:25 +0200

    Add project files.

---
+++ MediatRWithApi/Data/ContactsContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatRWithApi.Data
+{
+    public class ContactsContext : DbContext
+    {
+        public ContactsContext(DbContextOptions<ContactsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact { Id = 1, FirstName = "Mahmooud", LastName = "Reda" },
+                new Contact { Id = 2, FirstName = "Mahmoud", LastName = "Belal" },
+                new Contact { Id = 3, FirstName = "Belal", LastName = "Mahmoud" });
+        }
+    }
+
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
